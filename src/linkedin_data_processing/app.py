13063d1a399ac0d365cdc907f7258cdfde976a2d
Gabriel Caldas <gcaldasl2000@gmail.com>
2025-04-24 22:41:21 -0300

    Enhance LinkedIn scraping functionality with summary parsing
    - Updated `scrape_linkedin` function in `app.py` to use a mock profile URL for testing.
    - Changed the language model from `gpt-3.5-turbo` to `gpt-4o-mini`.
    - Integrated `summary_parser` to format the output, providing a structured summary and interesting facts.
    - Added `output_parsers.py` to define a `Summary` model for output parsing.

--- src/linkedin_data_processing/app.py
+++ src/linkedin_data_processing/app.py
@@ -2,31 +2,32 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 
 from linkedin_data_processing.agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from linkedin_data_processing.output_parsers import summary_parser
 from third_parties.linkedin import scrape_linkedin_profile
 
 def scrape_linkedin(name: str) -> str:
   linkedin_username = linkedin_lookup_agent(name=name)
   if linkedin_username:
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
-    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
     summary_template = """
     Given the information {information} about a person from I want you to create:
     1. a short summary
     2. three interesting facts about them
+
+    \n{format_instructions}
     """
 
     prompt = PromptTemplate(
       input_variables=["information"],
-      template=summary_template
+      template=summary_template,
+      partial_variables={"format_instructions": summary_parser.get_format_instructions()}
     )
 
-    chain = prompt | llm
-
+    chain = prompt | llm | summary_parser
     res = chain.invoke(input={"information": linkedin_data})
-
-    print(res.content)
 
 if __name__ == "__main__":
   scrape_linkedin(name="Jonathan Costa Prova Fácil")
